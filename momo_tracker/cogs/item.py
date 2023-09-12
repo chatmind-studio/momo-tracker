1eda9a250d25b13870f25ab874e0a1282fe5d4d2
seriaati <seria.ati@gmail.com>
2023-09-12 22:30:30 +0800

    feat: cogs

---
+++ momo_tracker/cogs/item.py
@@ -0,0 +1,123 @@
+from typing import Any, List
+
+from line import Cog, Context, command
+from line.models import (
+    ButtonsTemplate,
+    PostbackAction,
+    QuickReply,
+    QuickReplyItem,
+    URIAction,
+)
+from playwright.async_api import Browser
+from tortoise.exceptions import IntegrityError
+
+from ..bot_model import MomoTrackerBot
+from ..crawl.crawler import fetch_item_object
+from ..db_models import User
+from ..utils import split_list
+
+
+async def add_item_to_db(*, user_id: str, item_url: str, browser: Browser) -> str:
+    user = await User.get(id=user_id)
+
+    item = await fetch_item_object(browser, item_url)
+    try:
+        await item.save()
+    except IntegrityError:
+        pass
+    else:
+        await user.items.add(item)
+    return item.name
+
+
+class ItemCog(Cog):
+    def __init__(self, bot: MomoTrackerBot):
+        super().__init__(bot)
+        self.bot = bot
+
+    @command
+    async def view_items(self, ctx: Context, index: int = 0) -> Any:
+        user, _ = await User.get_or_create(id=ctx.user_id)
+
+        all_items = await user.items.all()
+        split_items = split_list(all_items, 11)
+        items = split_items[index]
+
+        quick_reply_items: List[QuickReplyItem] = []
+        for item in items:
+            quick_reply_item = QuickReplyItem(
+                PostbackAction(
+                    f"{item.name[:17]}..." if len(item.name) > 20 else item.name,
+                    data=f"cmd=view_item&item_id={item.id}",
+                )
+            )
+            quick_reply_items.append(quick_reply_item)
+        if index > 0:
+            quick_reply_items.insert(
+                0,
+                QuickReplyItem(
+                    action=PostbackAction(
+                        label="上一頁", data=f"cmd=view_items&index={index-1}"
+                    )
+                ),
+            )
+        if index < len(split_items) - 1:
+            quick_reply_items.append(
+                QuickReplyItem(
+                    action=PostbackAction(
+                        label="下一頁", data=f"cmd=view_items&index={index+1}"
+                    )
+                )
+            )
+        quick_reply = QuickReply(items=quick_reply_items)
+
+        template = ButtonsTemplate(
+            f"目前追蹤了 {len(all_items)} 個商品\n點按下方的按鈕來查看已追蹤商品的詳情",
+            [
+                PostbackAction(
+                    label="追蹤新的商品",
+                    data="ignore",
+                    fill_in_text="cmd=add_item&item_url=",
+                    input_option="openKeyboard",
+                    display_text="請直接貼上 momo 商品網址\n輸入後需等待 3~5 秒至程式成功獲取商品\n\n請勿更動前面的英文指令",
+                )
+            ],
+            title="追蹤清單",
+        )
+        await ctx.reply_template("追蹤清單", template=template, quick_reply=quick_reply)
+
+    @command
+    async def view_item(self, ctx: Context, item_id: str) -> Any:
+        user = await User.get(id=ctx.user_id)
+        item = await user.items.filter(id=item_id).first()
+        assert item
+
+        template = ButtonsTemplate(
+            item.name,
+            [
+                PostbackAction(
+                    label="取消追蹤",
+                    data=f"cmd=remove_item&item_id={item.id}",
+                ),
+                URIAction(
+                    label="前往商品頁面",
+                    uri=f"https://www.momoshop.com.tw/goods/GoodsDetail.jsp?i_code={item.id}&openExternalBrowser=1",
+                ),
+            ],
+        )
+        await ctx.reply_template(item.name, template=template)
+
+    @command
+    async def add_item(self, ctx: Context, item_url: str) -> Any:
+        item_name = await add_item_to_db(
+            user_id=ctx.user_id, item_url=item_url, browser=self.bot.browser
+        )
+        await ctx.reply_text(f"已將 {item_name} 加入追蹤清單")
+
+    @command
+    async def remove_item(self, ctx: Context, item_id: str) -> Any:
+        user = await User.get(id=ctx.user_id)
+        item = await user.items.filter(id=item_id).first()
+        assert item
+        await user.items.remove(item)
+        await ctx.reply_text(f"已取消追蹤 {item.name}")
---
+++ momo_tracker/cogs/set_notify.py
@@ -0,0 +1,43 @@
+from typing import Optional
+
+from line import Bot, Cog, Context, command
+from line.models import ButtonsTemplate, PostbackAction
+
+from ..db_models import User
+from ..utils import line_notify
+
+
+class SetNotifyCog(Cog):
+    def __init__(self, bot: Bot):
+        super().__init__(bot)
+        self.bot = bot
+
+    @command
+    async def set_line_notify(self, ctx: Context, token: Optional[str] = None):
+        user, _ = await User.get_or_create(id=ctx.user_id)
+        if not token:
+            if not user.line_notify_token:
+                display_text = "請直接貼上 LINE Notify 權杖\n\n請勿更動前面的英文指令"
+            else:
+                display_text = "你已經設定過 LINE Notify 權杖\n輸入新的權杖以更新"
+            template = ButtonsTemplate(
+                "如欲在追蹤的商品特價時收到通知, 請先設定 LINE Notify\n操作方式可查看「使用說明」",
+                [
+                    PostbackAction(
+                        "輸入 LINE Notify 權杖",
+                        data="ignore",
+                        fill_in_text="cmd=set_line_notify&token=",
+                        display_text=display_text,
+                        input_option="openKeyboard",
+                    )
+                ],
+            )
+            return await ctx.reply_template("設定 LINE Notify", template=template)
+
+        success = await line_notify(token, "測試訊息")
+        if not success:
+            return await ctx.reply_text("無效的 LINE Notify 權杖")
+
+        user.line_notify_token = token
+        await user.save()
+        await ctx.reply_text("LINE Notify 權杖設置成功")
