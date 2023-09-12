1594932f44bd941994f9aef86892b542754eb154
seriaati <seria.ati@gmail.com>
2023-09-12 22:30:10 +0800

    feat: db models

---
+++ momo_tracker/db_models.py
@@ -0,0 +1,27 @@
+from tortoise import fields
+from tortoise.models import Model
+
+
+class User(Model):
+    id = fields.CharField(pk=True, max_length=33)
+    line_notify_token = fields.CharField(max_length=255, null=True)
+    items: fields.ManyToManyRelation["Item"] = fields.ManyToManyField(
+        "models.Item", related_name="users", through="user_item"
+    )
+
+
+class Item(Model):
+    id = fields.CharField(pk=True, max_length=255)
+    name = fields.CharField(max_length=255)
+    users: fields.ManyToManyRelation[User]
+
+
+class PromotionItem(Model):
+    id = fields.CharField(pk=True, max_length=255)
+    url = fields.CharField(max_length=255)
+    original_price = fields.IntField()
+    discount_price = fields.IntField()
+    discount_rate = fields.IntField()
+    name = fields.CharField(max_length=255)
+    brand_name = fields.CharField(max_length=255)
+    remain_count = fields.IntField()
