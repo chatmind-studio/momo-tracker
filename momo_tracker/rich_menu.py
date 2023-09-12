3854deb251fdfe595f0c7a8f5891f3dfeb75534b
seriaati <seria.ati@gmail.com>
2023-09-12 22:29:53 +0800

    feat: rich menu

---
+++ momo_tracker/rich_menu.py
@@ -0,0 +1,34 @@
+from line.models import MessageAction, URIAction
+from linebot.v3.messaging import (
+    RichMenuArea,
+    RichMenuBounds,
+    RichMenuRequest,
+    RichMenuSize,
+)
+
+RICH_MENU = RichMenuRequest(
+    size=RichMenuSize(width=1200, height=810),
+    selected=True,
+    name="rich_menu",
+    chatBarText="點擊開啟/關閉選單",
+    areas=[
+        RichMenuArea(
+            bounds=RichMenuBounds(x=0, y=0, width=600, height=405),
+            action=MessageAction(text="cmd=view_items", label="追蹤商品"),
+        ),
+        RichMenuArea(
+            bounds=RichMenuBounds(x=600, y=0, width=600, height=405),
+            action=URIAction(
+                uri="https://www.momoshop.com.tw/main/Main.jsp", label="momo 官網"
+            ),
+        ),
+        RichMenuArea(
+            bounds=RichMenuBounds(x=0, y=405, width=600, height=405),
+            action=MessageAction(text="cmd=set_line_notify", label="通知設定"),
+        ),
+        RichMenuArea(
+            bounds=RichMenuBounds(x=600, y=405, width=600, height=405),
+            action=MessageAction(text="cmd=help", label="使用說明"),
+        ),
+    ],
+)
