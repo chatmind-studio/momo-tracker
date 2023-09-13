07b97923aa09917ef24de9f6db46e1338117008c
seriaati <seria.ati@gmail.com>
2023-09-13 08:50:00 +0800

    feat: show paginator in item detail

--- momo_tracker/cogs/item.py
+++ momo_tracker/cogs/item.py
@@ -11,7 +11,7 @@ from line.models import (
 from tortoise.exceptions import IntegrityError
 
 from ..crawler import fetch_item_object
-from ..db_models import User
+from ..db_models import Item, User
 from ..utils import extract_url, split_list
 
 
@@ -33,6 +33,40 @@ class ItemCog(Cog):
         super().__init__(bot)
         self.bot = bot
 
+    @staticmethod
+    async def item_paginator(items: List[Item], index: int) -> QuickReply:
+        split_items = split_list(items, 11)
+        items = split_items[index]
+
+        quick_reply_items: List[QuickReplyItem] = []
+        for item in items:
+            quick_reply_item = QuickReplyItem(
+                PostbackAction(
+                    f"{item.name[:17]}..." if len(item.name) > 20 else item.name,
+                    data=f"cmd=view_item&item_id={item.id}&index={index}",
+                )
+            )
+            quick_reply_items.append(quick_reply_item)
+        if index > 0:
+            quick_reply_items.insert(
+                0,
+                QuickReplyItem(
+                    action=PostbackAction(
+                        label="上一頁", data=f"cmd=view_items&index={index-1}"
+                    )
+                ),
+            )
+        if index < len(split_items) - 1:
+            quick_reply_items.append(
+                QuickReplyItem(
+                    action=PostbackAction(
+                        label="下一頁", data=f"cmd=view_items&index={index+1}"
+                    )
+                )
+            )
+        quick_reply = QuickReply(items=quick_reply_items)
+        return quick_reply
+
     @command
     async def view_items(self, ctx: Context, index: int = 0) -> Any:
         user, _ = await User.get_or_create(id=ctx.user_id)
@@ -53,43 +87,13 @@ class ItemCog(Cog):
         )
 
         if all_items:
-            split_items = split_list(all_items, 11)
-            items = split_items[index]
-
-            quick_reply_items: List[QuickReplyItem] = []
-            for item in items:
-                quick_reply_item = QuickReplyItem(
-                    PostbackAction(
-                        f"{item.name[:17]}..." if len(item.name) > 20 else item.name,
-                        data=f"cmd=view_item&item_id={item.id}",
-                    )
-                )
-                quick_reply_items.append(quick_reply_item)
-            if index > 0:
-                quick_reply_items.insert(
-                    0,
-                    QuickReplyItem(
-                        action=PostbackAction(
-                            label="上一頁", data=f"cmd=view_items&index={index-1}"
-                        )
-                    ),
-                )
-            if index < len(split_items) - 1:
-                quick_reply_items.append(
-                    QuickReplyItem(
-                        action=PostbackAction(
-                            label="下一頁", data=f"cmd=view_items&index={index+1}"
-                        )
-                    )
-                )
-            quick_reply = QuickReply(items=quick_reply_items)
-
+            quick_reply = await self.item_paginator(items=all_items, index=index)
             await ctx.reply_template("追蹤清單", template=template, quick_reply=quick_reply)
         else:
             await ctx.reply_template("追蹤清單", template=template)
 
     @command
-    async def view_item(self, ctx: Context, item_id: str) -> Any:
+    async def view_item(self, ctx: Context, item_id: str, index: int = 0) -> Any:
         user = await User.get(id=ctx.user_id)
         item = await user.items.filter(id=item_id).first()
         assert item
@@ -107,7 +111,9 @@ class ItemCog(Cog):
                 ),
             ],
         )
-        await ctx.reply_template(item.name, template=template)
+
+        quick_reply = await self.item_paginator(items=[item], index=index)
+        await ctx.reply_template(item.name, template=template, quick_reply=quick_reply)
 
     @command
     async def add_item(self, ctx: Context, item_url: str) -> Any:
