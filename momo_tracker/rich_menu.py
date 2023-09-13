eaf16aaba9c27c96d0c2bfbd2f3843c5cfe5ffa0
seriaati <seria.ati@gmail.com>
2023-09-13 20:47:11 +0800

    upd: replace message action with postback action

--- momo_tracker/rich_menu.py
+++ momo_tracker/rich_menu.py
@@ -1,4 +1,4 @@
-from line.models import MessageAction, URIAction
+from line.models import PostbackAction, URIAction
 from linebot.v3.messaging import (
     RichMenuArea,
     RichMenuBounds,
@@ -14,7 +14,7 @@ RICH_MENU = RichMenuRequest(
     areas=[
         RichMenuArea(
             bounds=RichMenuBounds(x=0, y=0, width=600, height=405),
-            action=MessageAction(text="cmd=view_items", label="追蹤商品"),
+            action=PostbackAction(data="cmd=view_items", label="追蹤商品"),
         ),
         RichMenuArea(
             bounds=RichMenuBounds(x=600, y=0, width=600, height=405),
@@ -25,7 +25,7 @@ RICH_MENU = RichMenuRequest(
         ),
         RichMenuArea(
             bounds=RichMenuBounds(x=0, y=405, width=600, height=405),
-            action=MessageAction(text="cmd=set_line_notify", label="通知設定"),
+            action=PostbackAction(data="cmd=set_line_notify", label="通知設定"),
         ),
         RichMenuArea(
             bounds=RichMenuBounds(x=600, y=405, width=600, height=405),
