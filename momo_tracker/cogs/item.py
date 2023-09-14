de8256afaaadabf8186326226bc1e43194e4afa6
seriaati <seria.ati@gmail.com>
2023-09-14 10:15:49 +0800

    feat: show items after removing

--- momo_tracker/cogs/item.py
+++ momo_tracker/cogs/item.py
@@ -1,4 +1,4 @@
-from typing import Any, List
+from typing import Any, List, Union
 
 from line import Bot, Cog, Context, command
 from line.models import (
@@ -7,6 +7,8 @@ from line.models import (
     PostbackAction,
     QuickReply,
     QuickReplyItem,
+    TemplateMessage,
+    TextMessage,
     URIAction,
 )
 from tortoise.exceptions import IntegrityError
@@ -32,66 +34,76 @@ class ItemCog(Cog):
         super().__init__(bot)
         self.bot = bot
 
-    @command
-    async def view_items(self, ctx: Context, index: int = 0) -> Any:
+    @staticmethod
+    async def view_items_message(
+        ctx: Context, index: int = 0
+    ) -> Union[TemplateMessage, TextMessage]:
         user, _ = await User.get_or_create(id=ctx.user_id)
         all_items = await user.items.all()
-
         if not all_items:
-            await ctx.reply_text(
+            return TextMessage(
                 "你尚未追蹤任何商品, 追蹤商品後才能在特價時收到通知\n追蹤方式: 將 momo 購物網商品頁面的網址分享給機器人\n\n(圖文教學可點擊「使用教學」查看)"
             )
-        else:
-            split_items = split_list(all_items, 10)
-            items = split_items[index]
-            columns: List[CarouselColumn] = []
-            for item in items:
-                column = CarouselColumn(
-                    text=f"{item.name[:57]}..." if len(item.name) > 60 else item.name,
-                    actions=[
-                        PostbackAction(
-                            "取消追蹤",
-                            data=f"cmd=remove_item&item_id={item.id}",
-                        ),
-                        URIAction(
-                            "前往商品頁面",
-                            uri=f"https://www.momoshop.com.tw/goods/GoodsDetail.jsp?i_code={item.id}&openExternalBrowser=1",
-                        ),
-                    ],
-                    thumbnail_image_url=item.image_url,
-                )
-                columns.append(column)
 
-            quick_reply_items: List[QuickReplyItem] = []
-            if index > 0:
-                quick_reply_items.append(
-                    QuickReplyItem(
-                        action=PostbackAction(
-                            label="上一頁", data=f"cmd=view_items&index={index-1}"
-                        )
+        split_items = split_list(all_items, 10)
+        items = split_items[index]
+        columns: List[CarouselColumn] = []
+        for item in items:
+            column = CarouselColumn(
+                text=f"{item.name[:57]}..." if len(item.name) > 60 else item.name,
+                actions=[
+                    PostbackAction(
+                        "取消追蹤",
+                        data=f"cmd=remove_item&item_id={item.id}",
                     ),
-                )
-            if index < len(split_items) - 1:
-                quick_reply_items.append(
-                    QuickReplyItem(
-                        action=PostbackAction(
-                            label="下一頁", data=f"cmd=view_items&index={index+1}"
-                        )
+                    URIAction(
+                        "前往商品頁面",
+                        uri=f"https://www.momoshop.com.tw/goods/GoodsDetail.jsp?i_code={item.id}&openExternalBrowser=1",
+                    ),
+                ],
+                thumbnail_image_url=item.image_url,
+            )
+            columns.append(column)
+
+        quick_reply_items: List[QuickReplyItem] = []
+        if index > 0:
+            quick_reply_items.append(
+                QuickReplyItem(
+                    action=PostbackAction(
+                        label="上一頁", data=f"cmd=view_items&index={index-1}"
+                    )
+                ),
+            )
+        if index < len(split_items) - 1:
+            quick_reply_items.append(
+                QuickReplyItem(
+                    action=PostbackAction(
+                        label="下一頁", data=f"cmd=view_items&index={index+1}"
                     )
                 )
-
-            await ctx.reply_template(
-                "追蹤清單",
-                template=CarouselTemplate(columns=columns),
-                quick_reply=QuickReply(items=quick_reply_items)
-                if quick_reply_items
-                else None,
             )
+
+        return TemplateMessage(
+            "追蹤清單",
+            template=CarouselTemplate(columns=columns),
+            quick_reply=QuickReply(items=quick_reply_items)
+            if quick_reply_items
+            else None,
+        )
+
+    @command
+    async def view_items(self, ctx: Context, index: int = 0) -> Any:
+        message = await self.view_items_message(ctx, index)
+        await ctx.reply_multiple([message])
 
     @command
     async def remove_item(self, ctx: Context, item_id: str) -> Any:
         user = await User.get(id=ctx.user_id)
+
         item = await user.items.filter(id=item_id).first()
         assert item
         await user.items.remove(item)
-        await ctx.reply_text(f"已取消追蹤 {item.name}")
+
+        await ctx.reply_multiple(
+            [TextMessage(f"已取消追蹤 {item.name}"), await self.view_items_message(ctx)]
+        )
